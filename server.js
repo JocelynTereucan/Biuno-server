b15ee0bd6a925a939647f0f046a024334ffe5277
JocelynTereucan <38298509+JocelynTereucan@users.noreply.github.com>
2025-05-12 12:11:39 -0400

    actualizacion

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 10000;
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
