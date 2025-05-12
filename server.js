f835ec1040974ca4d17a6829ba7fc90c133f8536
JocelynTereucan <38298509+JocelynTereucan@users.noreply.github.com>
2025-05-12 05:29:25 -0400

    conexion a firebase

--- server.js
+++ server.js
@@ -3,63 +3,60 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 10000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebaseKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
 
 app.get("/", (req, res) => {
-    res.send("¡Servidor de Biuno funcionando!");
+  res.send("¡Servidor de Biuno funcionando!");
 });
 
-// Nueva ruta POST
 app.post("/data", (req, res) => {
-    const {
-        actividadId,
-        childId,
-        respuestaSeleccionada,
-        esCorrecta,
-        duracionSegundos,
-        vecesEscuchoInstruccion,
-        timestamp,
-        imagenBase64
-    } = req.body;
+  const {
+    actividadId,
+    childId,
+    respuestaSeleccionada,
+    esCorrecta,
+    duracionSegundos,
+    vecesEscuchoInstruccion,
+    timestamp,
+    imagenBase64
+  } = req.body;
 
-    if (!childId || !respuestaSeleccionada || !timestamp) {
-        return res.status(400).json({ error: "Faltan datos obligatorios" });
-    }
+  if (!childId || !respuestaSeleccionada || !timestamp) {
+    return res.status(400).json({ error: "Faltan datos obligatorios" });
+  }
 
-    const nuevoResultado = {
-        actividadId,
-        childId,
-        respuestaSeleccionada,
-        esCorrecta,
-        duracionSegundos,
-        vecesEscuchoInstruccion,
-        timestamp,
-        imagenBase64
-    };
+  const nuevoResultado = {
+    actividadId,
+    childId,
+    respuestaSeleccionada,
+    esCorrecta,
+    duracionSegundos,
+    vecesEscuchoInstruccion,
+    timestamp,
+    imagenBase64
+  };
 
-    const filePath = path.join(__dirname, "resultados.json");
-
-    // Leer archivo anterior (si existe)
-    let resultados = [];
-    if (fs.existsSync(filePath)) {
-        const contenido = fs.readFileSync(filePath, "utf8");
-        resultados = contenido ? JSON.parse(contenido) : [];
-    }
-
-    // Agregar nuevo resultado
-    resultados.push(nuevoResultado);
-
-    // Guardar de nuevo
-    fs.writeFileSync(filePath, JSON.stringify(resultados, null, 2));
-
-    res.json({ message: "Datos recibidos y guardados exitosamente" });
+  db.collection("resultados").add(nuevoResultado)
+    .then(() => {
+      res.json({ message: "Datos guardados en Firebase exitosamente" });
+    })
+    .catch((error) => {
+      console.error("Error al guardar en Firebase:", error);
+      res.status(500).json({ error: "Error al guardar en Firebase" });
+    });
 });
-
-
-
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
